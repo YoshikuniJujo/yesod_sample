ba3fcb080eadcfcd2fa7ae6b20749a783ca9ba5f
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-03-31 08:52:12 +0000

    add for TLS

--- app/main.hs
+++ app/main.hs
@@ -1,8 +1,34 @@
-import Prelude              (IO)
-import Yesod.Default.Config (fromArgs)
-import Yesod.Default.Main   (defaultMainLog)
+import Prelude              (IO, ($), (>>=), (.), print)
+
+import TLSSocket
+
+import Yesod.Default.Config -- (fromArgs)
+import Yesod.Default.Main   (defaultMain)
 import Settings             (parseExtra)
 import Application          (makeApplication)
 
+-- import Network.Wai.Handler.WarpTLS
+import Network.Wai.Handler.Warp
+import Data.Conduit.Network
+import Network
+import Control.Exception
+import System.Posix
+
+-- tlss = tlsSettings
+crt = "/home/tatsuya/csr/2014crt_cross_owl_skami3.pem"
+key = "/home/tatsuya/csr/mydomain.key"
+
 main :: IO ()
-main = defaultMainLog (fromArgs parseExtra) makeApplication
+main = do -- defaultMain (fromArgs parseExtra) makeApplication
+	config <- load
+	(app, _) <- getApp config
+	bracket (bindPort (appPort config) (appHost config)) sClose $ \socket -> do
+		getGroupEntryForName "yesod" >>= setGroupID . groupID
+		getUserEntryForName "yesod" >>= setUserID . userID
+		runTLSSocket crt key defaultSettings {
+			settingsPort = appPort config,
+			settingsHost = appHost config
+		 } socket app
+	where
+	load = fromArgs parseExtra
+	getApp = makeApplication
